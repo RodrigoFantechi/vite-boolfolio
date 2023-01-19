edf4d7140043e647e00d9361004a08f716ed1786
RodrigoFantechi <rodrifante595@libero.it>
2023-01-19 17:35:44 +0100

    add navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script>
 import { store } from './store.js'
 import ProjectCard from './components/ProjectCard.vue';
-
+import Appheader from './components/Appheader.vue';
+import Appfooter from './components/Appfooter.vue';
 export default {
-    components:{
+    components: {
         ProjectCard,
+        Appheader,
+        Appfooter,
     },
     data() {
         return {
@@ -13,31 +16,33 @@ export default {
     },
 
     mounted() {
-   store.callAxios(store.url);
-  }
+        store.callAxios(store.url);
+    }
 }
 </script>
 
 <template>
-    <div class="container">
-        <section v-if="store.projects">
-            <div class="row">
-                    <ProjectCard v-for="project in store.projects"
-                        :title='project.title'
-                        :cover_image='project.cover_image'
-                        :description='project.description'
-                        :type='project.type'
-                        :technologies='project.technologies'
-                        />
-            </div>
+    <header>
+        <Appheader></Appheader>
+    </header>
+    <main>
+        <div class="container p-5">
+            <section v-if="store.projects">
+                <div class="row">
+                    <ProjectCard v-for="project in store.projects" :title='project.title'
+                        :cover_image='project.cover_image' :description='project.description' :type='project.type'
+                        :technologies='project.technologies' />
+                </div>
 
-        </section>
-        <section v-else>
-            <div class="no_records">
-                <h4>Sorry no records inside the database.</h4>
-            </div>
-        </section>
-    </div>
+            </section>
+            <section v-else>
+                <div class="no_records">
+                    <h4>Sorry no records inside the database.</h4>
+                </div>
+            </section>
+        </div>
+    </main>
+    <Appfooter></Appfooter>
 </template>
 
 <style lang="scss">
