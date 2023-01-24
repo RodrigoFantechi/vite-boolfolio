39578d247f85ea8eab76626e0d37c7074b8dd0c8
RodrigoFantechi <rodrifante595@libero.it>
2023-01-24 13:03:24 +0100

    add bg

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -21,20 +21,20 @@ export default {
 </script>
 <template >
     <div class="col">
-        <div class="card h-100 text-center p-3">
+        <div class="card h-100 text-center p-3 shadow-sm">
             <div class="image mx-auto">
                 <img width="150" class="card-image rounded-top" :src="store.getImagePath(cover_image)" alt="">
             </div>
             <div class="details my-4">
                 <h4>{{ title }}</h4>
-                <p>{{ store.checkText(description) }}</p>
+                <p class="">{{ store.checkText(description) }}</p>
                 <p v-if="type">Tipo: {{ type.name }}</p>
-                <p v-else>Tipo: Nessun tipo associato</p>
+                <p class="text-muted" v-else>Tipo: Nessun tipo associato</p>
                 <div v-if="technologies.length > 0" class="my-3">
                     <span>Tecnologie:</span>
                     <span v-for="singletechnology in technologies" class="mx-1">#{{ singletechnology.name }}</span>
                 </div>
-                <p v-else >Tecnologie: Nessuna tecnologia associata</p>
+                <p class="text-muted" v-else >Tecnologie: Nessuna tecnologia associata</p>
                 <router-link class="btn btn-primary" :to="{ name: 'single-project', params: { slug: slug } }">Read more</router-link>
             </div>
         </div>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -13,10 +13,10 @@ export default {
 </script>
 <template>
 
-
-  <div class="container py-5">
-    <div class="row align-items-center g-5 py-5">
-      <div class="col-lg-6">
+  <section class="top bg-light">
+    <div class="container py-5">
+      <div class="row align-items-center g-5 py-5">
+        <div class="col-lg-6">
           <h1 class="display-5 fw-bold lh-1 mb-3">Home Page Portfolio</h1>
           <p class="lead"> Dai primi progetti autodidattici in Java con Java Swing, posso finalmente presentare un
             portfolio "completo". Tra le tecnologie e i linguaggi utilizzati troviamo: Php, html, css, scss, Vue, Js,
@@ -26,17 +26,25 @@ export default {
             <a type="button" href="#" class="btn btn-primary btn-lg px-4 me-md-2">To Github</a>
             <a type="button" href="#" class="btn btn-outline-secondary btn-lg px-4">Send Message</a>
           </div>
+        </div>
       </div>
     </div>
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <AppCarousel />
+  </section>
+  <section class="bottom">
+    <div class="container py-5">
+      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+          <AppCarousel />
+        </div>
+        <div class="col-lg-6">
+          <h2 > Esempio di carousel da fixare --></h2>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="slider">
-    <AppSlider />
-  </div>
+    <div class="slider">
+      <AppSlider />
+    </div>
+  </section>
 </template>
 
 
